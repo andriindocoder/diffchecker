e2d311f4bd4998b2369af44cdd52d725f22fe76b
Andri Wicaksono <andri.wicaksono@appfuxion.id>
2025-02-20 11:33:30 +0700

    Api inventory - update-yaml.sh

--- shell/api-catalogue/update-yaml.sh
+++ shell/api-catalogue/update-yaml.sh
@@ -17,10 +17,19 @@ fi
 # Create a temporary backup of the original YAML file
 cp "$FILE_TO_UPDATE" "${FILE_TO_UPDATE}.bak"
 
-# Remove the existing spring, server.port, and springdoc content in the YAML file, but keep other parts intact
-awk '/^spring:/ {flag_spring=1; next} /^server:/ {flag_server=1; next} /^springdoc:/ {flag_springdoc=1; next} /^[^ ]/ {flag_spring=0; flag_server=0; flag_springdoc=0} !flag_spring && !flag_server && !flag_springdoc {print}' "$FILE_TO_UPDATE" > temp.yaml
+# Remove all comments (#) inside the application-local YAML file
+sed '/^\s*#/d' "$FILE_TO_UPDATE" > temp.yaml
+mv temp.yaml "$FILE_TO_UPDATE"
 
-# Write the new spring content to the temporary file
+# Remove the existing spring, server.port, springdoc, and cloud content in the YAML file, but keep other parts intact
+awk '/^spring:/ {flag_spring=1; next} 
+     /^server:/ {flag_server=1; next} 
+     /^springdoc:/ {flag_springdoc=1; next} 
+     /^cloud:/ {flag_cloud=1; next} 
+     /^[^ ]/ {flag_spring=0; flag_server=0; flag_springdoc=0; flag_cloud=0} 
+     !flag_spring && !flag_server && !flag_springdoc && !flag_cloud {print}' "$FILE_TO_UPDATE" > temp.yaml
+
+# Write the new spring and cloud content to the temporary file
 cat <<'EOF' >> temp.yaml
 spring:
   main:
@@ -40,6 +49,11 @@ spring:
     console:
       enabled: true
       path: /h2-console
+  autoconfigure:
+    exclude:
+      - org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration
+      - org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration
+      - org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration
 
 server:
   port: 8080
@@ -51,6 +65,15 @@ springdoc:
     path: /internal/swagger-doc/v3/api-docs
   swagger-ui:
     path: /internal/swagger-doc/swagger-ui.html
+
+cloud:
+  aws:
+    stack:
+      auto: false
+    region:
+      auto: false
+    credentials:
+      instance-profile: false
 EOF
 
 # Move the temp file to the original file
